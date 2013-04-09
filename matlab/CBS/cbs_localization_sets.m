c07400e34d4e681a0ea88cd25ed07e7d95d25cb5
Jiapeng Huang <jiapeng@jiapeng.(none)>
2013-04-09 12:19:42 -0400

    temp

--- matlab/CBS/cbs_localization_sets.m
+++ matlab/CBS/cbs_localization_sets.m
@@ -3,15 +3,18 @@ clear;
 close all;
 k = 9.9417e+06;
 % number of sets of localization
-NSET = 10;
+NSET = 5;
+% number of iteration
+NI = 3600;
+mean_free_path = 18e-6;
 field = 0;
 field_abs = 0;
 rand_x = randn(1,NSET);
 rand_y = randn(1,NSET);
 % For path 1, light is coming in the orignal point and coming out at (1,1,0.01) point. 
 for i=1: NSET
-point_out = [rand_x(i)*2.0e-06  2.0e-06*rand_y(i) 0.01e-06];
-phi = linspace(-pi,pi,3600);
+point_out = [rand_x(i)*2.0e-06  2.0e-06*rand_y(i) 0.0e-06];
+phi = linspace(-pi,pi,NI);
 phase_1_in = 0;
 cos_theta = 0.848;
 sin_theta = 0.53;
@@ -24,10 +27,11 @@ phase_2_out = 0;
 % show the plot
 field_1 = exp(k*1i*(phase_1_in + phase_1_out));
 field_2 = exp(k*1i*(phase_2_in + phase_2_out));
-field = field_1 + field_2;
 % here we just assume that for different sets of localization that the
 % phase is same. This is also "like" the situation in the experiment since
 % the phase shift is mostly determined by the mean free path. 
+path = pathlength_creator(mean_free_path,NI);
+field = (field_1 + field_2);
 field_abs = abs(field) + field_abs;
 end
-polar(phi,field_abs);
+polar(phi,field_abs.^2);
